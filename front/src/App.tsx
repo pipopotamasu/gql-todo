84cb5fc23fb08fb185a0b2b3846968ca0f369363
pipopotamasu <ec0156hx@gmail.com>
2020-06-15 22:25:02 +0900

    server front connection

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ApolloClient from 'apollo-boost';
+import { gql } from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+});
+
 
 function App() {
+  useEffect(() => {
+    client
+      .query({
+              query: gql`
+                {
+                  todos {
+                    id,
+                    content,
+                    done
+                  }
+                }
+              `
+            })
+  .then(result => console.log(result));
+
+  })
+
   return (
     <div className="App">
       foo
