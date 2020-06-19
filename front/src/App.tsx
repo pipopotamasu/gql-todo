c54e9229d97cd7acaf05c9f0855b1b93758e92c5
pipopotamasu <ec0156hx@gmail.com>
2020-06-19 20:04:26 +0900

    Todos (#13)
    * todos
    
    * todos

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { client } from './apollo';
-import TodoApp from './components/TodoApp';
+
+const TodoApp = lazy(() => import('./components/TodoApp'));
 
 
 function App() {
   return (
     <div className="App">
       <ApolloProvider client={client}>
-        <TodoApp />
+        <Suspense fallback={<p>Loading...</p>}>
+          <TodoApp />
+        </Suspense>
       </ApolloProvider>
     </div>
   );
--- front/src/components/TodoApp.tsx
+++ front/src/components/TodoApp.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import React, { useMemo, useState, useCallback, ChangeEvent} from 'react';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+
+type Todo = {
+  id: number,
+  user: { id: number },
+  content: string,
+  done: boolean
+}
 
 const FETCH_TODOS = gql`
   {
@@ -15,17 +22,56 @@ const FETCH_TODOS = gql`
   }
 `;
 
-function TodoApp() {
-  const { loading, error, data } = useQuery(FETCH_TODOS);
+const ADD_TODO = gql`
+  mutation AddTodo($content: String!, $userId: Int!) {
+    addTodo(input: { content: $content, userId: $userId }) {
+      id
+      user {
+        id
+      }
+      content
+      done
+    }
+  }
+`;
 
-  if (loading) return <p>Loading...</p>;
+function TodoApp() {
+  const { error, data } = useQuery<{todos: Todo[]}>(FETCH_TODOS);
+  const [addTodo, { data: _data }] = useMutation(ADD_TODO);
+  const [newContent, setNewContent] = useState('');
+  const onClickAdd = useCallback(async () => {
+    const res = await addTodo({ variables: { content: newContent, userId: 1 } });
+    console.log(res)
+    console.log(1)
+  }, [addTodo, newContent]);
+  const onChangeInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setNewContent(e.target.value)
+  }, [setNewContent])
+
+  const Items = useMemo(() => {
+    if (!data) return <p>no data.</p>;
+    return data.todos.map((item) => {
+      return (
+        <li key={item.id}>
+          <input type="checkbox" checked={item.done} onChange={() => {}} />
+          <span>{ item.content }</span>
+        </li>
+      )
+    })
+  }, [data])
+
   if (error) return <p>Error :(</p>;
 
-  console.log(data)
   return (
-    <div>
-      hoge
-    </div>
+    <>
+      <div>
+        <input type="text" value={newContent} onChange={onChangeInput} />
+        <button type="button" onClick={onClickAdd}>Add</button>
+      </div>
+      <ul>
+        {Items}
+      </ul>
+    </>
   );
 }
 
