257f9f8b8da31a3ffbbe0164394e383f498055b3
pipopotamasu <ec0156hx@gmail.com>
2020-07-04 15:40:45 +0900

    fix schema

--- server/schema.js
+++ server/schema.js
@@ -2,17 +2,16 @@ const { gql } = require('apollo-server-express');
 
 module.exports = gql`
   type Query {
-    todo(id: Int!): Todo
+    todo(id: Int!): Todo!
     todos(searchBy: String = "", filter: TodoFilter = ALL, limit: Int = 10): [Todo!]!
     todoTotalCount: Int!
-    hello: String
   }
 
   type Mutation {
-    toggleTodo(id: Int!): Todo
-    addTodo(input: TodoCreateInput!): Todo
-    updateTodoContent(input: TodoUpdateInput!): Todo
-    deleteTodo(id: Int!): Todo
+    toggleTodo(id: Int!): Todo!
+    addTodo(input: TodoCreateInput!): Todo!
+    updateTodoContent(input: TodoUpdateInput!): Todo!
+    deleteTodo(id: Int!): Todo!
   }
 
   input TodoCreateInput {
